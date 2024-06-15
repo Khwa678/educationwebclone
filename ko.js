1e7889bcfdc545473fe67ab3e609b1ea5d09bf27
Khwahish Seth <148669282+Khwa678@users.noreply.github.com>
2024-06-15 20:07:40 +0530

    Add files via upload

---
+++ ko.js
@@ -0,0 +1,11 @@
+const burger=document.querySelector('.burger')
+const navbar=document.querySelector('.navbar')
+const navlist =document.querySelector('.nav-list')
+const rightNav=document.querySelector('.rightNav')
+burger.addEventListener('click',()=>{
+nav-list.classList.toggle('.visible');
+rightNav.classList.toggle('.visible');
+navbar.classList.toggle('.navheight');
+
+
+});
